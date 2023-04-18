003ec9ff778ed08acb503597548722bc1b8e42ec
Victor Del Carpio <vhdelcg@gmail.com>
2023-04-18 13:24:22 -0400

    add support for dall-e

--- handler.py
+++ handler.py
@@ -10,6 +10,9 @@ import os
 load_dotenv()
 
 PREFIX = '/'
+CHAT = PREFIX + "chat"
+DRAW = PREFIX + "draw"
+
 openai.api_key = os.environ.get('API_KEY')
 
 def receive(event, context):
@@ -32,10 +35,19 @@ def process(message):
         return None
     
     if message['text'].startswith(PREFIX):
-        messages = [{"role": "system", "content": "You are a helpful assistant."}, 
-                    {"role": "user", "content": message['text'][len(PREFIX):].strip()}]
-        response = openai.ChatCompletion.create(model="gpt-4", messages=messages)
-        reply = response.choices[0].message.content
+        reply = "Sorry that's not a known command, try /chat or /draw."
+
+        if message['text'].startswith(CHAT):
+            messages = [{"role": "system", "content": "You are a helpful assistant."}, 
+                        {"role": "user", "content": message['text'][len(CHAT):].strip()}]
+            response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+            reply = response.choices[0].message.content
+            
+        if message['text'].startswith(DRAW):
+            prompt = message['text'][len(DRAW):].strip()
+            response = openai.Image.create(prompt=prompt, n=1, size="256x256")
+            reply = response["data"][0]["url"]
+        
         return reply        
 
 
